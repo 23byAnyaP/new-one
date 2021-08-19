7016245285f6cbd2b02c3cf215ee0485a1b216d8
Anna <87009613+23byAnyaP@users.noreply.github.com>
2021-08-19 14:21:44 +0400

    Add files via upload

---
+++ task7/7.js
@@ -0,0 +1,15 @@
+function gcd_two_numbers(x, y) {
+  if ((typeof x !== 'number') || (typeof y !== 'number')) 
+    return false;
+  x = Math.abs(x);
+  y = Math.abs(y);
+  while(y) {
+    var t = y;
+    y = x % y;
+    x = t;
+  }
+  return x;
+}
+
+alert( gcd_two_numbers(24, 25) );
+alert( gcd_two_numbers(9, 3) );
